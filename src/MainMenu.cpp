afdf6a7d38dd35654432567478ed50e1c2b68984
kameHame HA <80411896+kamehame-ha@users.noreply.github.com>
2025-04-25 20:26:43 +0200

    Created MainMenu class

---
+++ src/MainMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mrlek on 25.04.2025.
+//
+
+#include "MainMenu.h"
---
+++ src/MainMenu.h
@@ -0,0 +1,16 @@
+//
+// Created by mrlek on 25.04.2025.
+//
+
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+
+
+class MainMenu {
+
+};
+
+
+
+#endif //MAINMENU_H
