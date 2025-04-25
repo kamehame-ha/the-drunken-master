9f34063fc050400665db65515935688262121187
kameHame HA <80411896+kamehame-ha@users.noreply.github.com>
2025-04-25 20:01:56 +0200

    Simplified game main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "src/Game.h"
+#include <fmt/base.h>
+
 int main() {
-    auto game = Game();
-    game.run();
+    try {
+        auto game = Game();
+        game.run();
+    } catch (...) {
+        fmt::print(stderr, "Exception occurred\n");
+    }
 }
