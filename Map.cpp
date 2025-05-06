73104d428c228ae08758d386739eb0bc1f68b78c
kamehame-ha <bartek@kluz.pro>
2025-05-06 12:18:51 +0200

    Map class initial setup

---
+++ Map.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kame on 5/6/25.
+//
+
+#include "Map.h"
---
+++ Map.h
@@ -0,0 +1,16 @@
+//
+// Created by kame on 5/6/25.
+//
+
+#ifndef MAP_H
+#define MAP_H
+
+
+
+class Map {
+
+};
+
+
+
+#endif //MAP_H
