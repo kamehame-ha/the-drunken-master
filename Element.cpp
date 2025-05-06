0f943d1fc0261be72eb1a66afff0d157dcfcf4dd
kamehame-ha <bartek@kluz.pro>
2025-05-06 12:19:35 +0200

    Element class initial setup

---
+++ Element.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kame on 5/6/25.
+//
+
+#include "Element.h"
---
+++ Element.h
@@ -0,0 +1,16 @@
+//
+// Created by kame on 5/6/25.
+//
+
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+
+
+class Element {
+
+};
+
+
+
+#endif //ELEMENT_H
